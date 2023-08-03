6d19f460156dab203313bad817f423d3f743f98c
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-08-04 02:50:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ for i in range(len(numbers2)):
     try:
         numbers2[i] = float(numbers2[i])
     except:
-        numbers2.remove(numbers2[i])
+        numbers2.pop(numbers2[i])
         
 st.header('검색된 숫자 목록')
 st.write(numbers2)
