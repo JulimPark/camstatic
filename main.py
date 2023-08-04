d888fa4a93fed44499c985cde3f3adb80b3c6bf0
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-08-05 00:26:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if rad == 'use cam':
     picture = st.camera_input('사진촬영')
 else:
     picture = st.file_uploader('파일을 선택하세요')
-try:
+# try:
     image = Image.open(picture)
     st.image(image)
     # 이미지 OCR
@@ -42,5 +42,5 @@ try:
     st.subheader(f":green[표준편차]: :blue[{format(np.std(numbers2),',')}]")
     st.subheader(f":green[분산]: :blue[{format(np.var(numbers2),',')}]")
     st.warning(':red[컴퓨터 비전 기술로 읽은 데이터에 대한 통계입니다. 인식환경에 따라 오차가 발생할 수 있으니 주의하시기 바랍니다.]')
-except:
-    st.error('숫자가 들어 있는 사진을 촬영하거나 사진 파일을 업로드 하세요.')
+# except:
+#     st.error('숫자가 들어 있는 사진을 촬영하거나 사진 파일을 업로드 하세요.')
